dd718218a5cb365443bb31fcc67a168895e38e4e
Raza ur Rehman <121747444+Raza-Ur-Rehman@users.noreply.github.com>
2023-09-14 20:08:58 +0500

    Add files via upload

---
+++ Stop Watch/script.js
@@ -0,0 +1,60 @@
+let seconds = 0; 
+let tens = 0; 
+let getSeconds = document.querySelector('.seconds');
+let getTens = document.querySelector('.tens');
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
+let interval;
+
+btnStart.addEventListener('click',()=>{
+    inverval = setInterval(startTimer, 10);
+}) 
+btnStop.addEventListener('click',()=>{
+    clearInterval(inverval);
+}) 
+btnReset.addEventListener('click',()=>{
+    clearInterval(inverval);
+    tens = '00';
+    seconds ='00';
+    getSeconds.innerHTML = seconds;
+    getTens.innerHTML = tens;
+}) 
+
+
+function startTimer(){
+    tens++;
+    if(tens <= 9){
+        getTens.innerHTML = '0' + tens;
+    }
+    if(tens > 9){
+        getTens.innerHTML = tens;
+    }
+    if(tens > 99){
+        seconds++;
+        getSeconds.innerHTML = '0' + seconds;
+        tens = 0;
+        getTens.innerHTML = '0' + 0;
+    }
+    if(seconds > 9){
+        getSeconds.innerHTML = seconds;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
